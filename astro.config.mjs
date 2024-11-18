49e426b7e9af5dc8dfff8aefe91d735f10662e34
Tobias Maestrini <tobias@bee365.ch>
2024-11-19 00:48:40 +0100

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
+import pagefind from "astro-pagefind";
+
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://tmaestrini.github.com',
+  build: {
+    format: "file",
+  },
+  integrations: [mdx(), sitemap(), tailwind(), pagefind(), react()],
+});
---
+++ src/components/Teaser.tsx
@@ -0,0 +1,139 @@
+
+export default function Example() {
+  return (
+    <>
+    {/* <section className="">
+      <div className="py-12 bg-base-200 rounded-lg">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="lg:text-start">
+            <h2
+              className="font-heading mb-4 bg-orange-100 text-orange-800 px-4 py-2 rounded-lg md:w-64 md:mx-auto text-xs font-semibold tracking-widest text-black uppercase title-font">
+              Why choose us?
+            </h2>
+            <p className="font-heading mt-2 text-3xl leading-8 font-semibold tracking-tight text-gray-900 sm:text-4xl">
+              We know tech, we know finance. We are fintech experts.
+            </p>
+            <p className="mt-4 max-w-2xl text-lg text-gray-500">
+              We know how to handle taxation for all the
+              countried we operate in. We care for our customers and help them manage cashflows.
+            </p>
+          </div>
+
+          <div className="mt-10">
+            <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+              <div className="relative">
+                <dt>
+                  <div
+                    className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-500 text-white">
+                    <img src="https://www.svgrepo.com/show/503163/api-settings.svg" />
+                  </div>
+                  <p className="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">Powerful API</p>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500">
+                  Lorem ipsum, dolor sit amet consectetur
+                  adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate
+                  blanditiis ratione.
+                </dd>
+              </div>
+              <div className="relative">
+                <dt>
+                  <div
+                    className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-500 text-white">
+                    <img src="https://www.svgrepo.com/show/503138/webpack.svg" />
+                  </div>
+                  <p className="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">Easy to integrate
+                    SDK
+                  </p>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500"> Lorem ipsum, dolor sit amet consectetur
+                  adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate
+                  blanditiis ratione.
+                </dd>
+              </div>
+              <div className="relative">
+                <dt>
+                  <div
+                    className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-500 text-white">
+                    <img src="https://www.svgrepo.com/show/511771/dashboard-671.svg" />
+
+                  </div>
+                  <p className="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">Low Transaction Cost
+                  </p>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500"> Lorem ipsum, dolor sit amet consectetur
+                  adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate
+                  blanditiis ratione.
+                </dd>
+              </div>
+              <div className="relative">
+                <dt>
+                  <div
+                    className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-primary-500 text-white">
+                    <img src="https://www.svgrepo.com/show/76267/free-commercial-label.svg" />
+
+                  </div>
+                  <p className="font-heading ml-16 text-lg leading-6 font-bold text-gray-700">Powerful Dashboard
+                  </p>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500"> Lorem ipsum, dolor sit amet consectetur
+                  adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate
+                  blanditiis ratione.
+                </dd>
+              </div>
+            </dl>
+          </div>
+
+        </div>
+      </div>
+    </section> */}
+
+    <div className="flex flex-row justify-between gap-8 mt-8">
+      <div
+        className="flex-1 rounded-lg bg-base-200
+        text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white">
+        <div
+          className="rounded-lg border-neutral-100 bg-zinc-50 p-4 dark:border-white/10 dark:bg-neutral-700">
+          Latest from Topics
+        </div>
+        <ul className="w-full">
+          <li
+            className="w-full border-b-2 border-neutral-100 border-opacity-100 px-4 py-3 dark:border-white/10">
+            Cras justo odio
+          </li>
+          <li
+            className="w-full border-b-2 border-neutral-100 border-opacity-100 px-4 py-3 dark:border-white/10">
+            Dapibus ac facilisis in
+          </li>
+          <li
+            className="w-full border-b-2 border-neutral-100 border-opacity-100 px-4 py-3 dark:border-white/10">
+            Vestibulum at eros
+          </li>
+        </ul>
+      </div>
+      
+      <div
+        className="flex-1 rounded-lg bg-base-200
+        text-surface shadow-secondary-1 dark:bg-surface-dark dark:text-white">
+        <div
+          className="rounded-lg border-neutral-100 bg-zinc-50 p-4 dark:border-white/10 dark:bg-neutral-700">
+          Latest from Topics  
+        </div>
+        <ul className="w-full">
+          <li
+            className="w-full border-b-2 border-neutral-100 border-opacity-100 px-4 py-3 dark:border-white/10">
+            Cras justo odio
+          </li>
+          <li
+            className="w-full border-b-2 border-neutral-100 border-opacity-100 px-4 py-3 dark:border-white/10">
+            Dapibus ac facilisis in
+          </li>
+          <li
+            className="w-full border-b-2 border-neutral-100 border-opacity-100 px-4 py-3 dark:border-white/10">
+            Vestibulum at eros
+          </li>
+        </ul>
+      </div>
+    </div>    
+    </>
+    );
+}
